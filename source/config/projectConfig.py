af544aad373b7c72d3f4a7ccbaf12841a32f71ae
YiFanZhang <455236930@qq.com>
2020-02-24 18:09:34 +0800

    fixbug

--- source/config/projectConfig.py
+++ source/config/projectConfig.py
@@ -4,15 +4,15 @@ import os
 
 class projectConfig:
     projectName = 'review'
-    PATH_CONFIG = os.sep + 'source' + os.sep + 'config' + os.sep + 'config.ini'
-    PATH_TEST_INPUT_EXCEL = os.sep + 'data' + os.sep + 'Test200.xlsx'
-    PATH_TEST_OUTPUT_EXCEL = os.sep + 'data' + os.sep + 'output.xlsx'
-    PATH_TEST_OUTPUT_PATH = os.sep + 'data'
-    PATH_STOP_WORD_HGD = os.sep + 'data' + os.sep + 'HGDStopWord.txt'
-    PATH_SPLIT_WORD_EXCEL = os.sep + 'data' + os.sep + 'output_splitword.xlsx'
-    PATH_USER_DICT_PATH = os.sep + 'data' + os.sep + 'user_dict.utf8'
-    PATH_TEST_CRF_INPUT = os.sep + 'data' + os.sep + 'people-daily.txt'
-    PATH_TEST_CRF_TEST_RESULT = os.sep + 'data' + os.sep + 'test.rst'
+    PATH_CONFIG = 'source' + os.sep + 'config' + os.sep + 'config.ini'
+    PATH_TEST_INPUT_EXCEL = 'data' + os.sep + 'Test200.xlsx'
+    PATH_TEST_OUTPUT_EXCEL = 'data' + os.sep + 'output.xlsx'
+    PATH_TEST_OUTPUT_PATH = 'data'
+    PATH_STOP_WORD_HGD = 'data' + os.sep + 'HGDStopWord.txt'
+    PATH_SPLIT_WORD_EXCEL = 'data' + os.sep + 'output_splitword.xlsx'
+    PATH_USER_DICT_PATH = 'data' + os.sep + 'user_dict.utf8'
+    PATH_TEST_CRF_INPUT = 'data' + os.sep + 'people-daily.txt'
+    PATH_TEST_CRF_TEST_RESULT = 'data' + os.sep + 'test.rst'
     TEST_OUT_PUT_SHEET_NAME = 'sheet1'
 
     @staticmethod
