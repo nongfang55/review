7e9c5d7c03bbc280c2f96e91d3f7d1f7d8f56704
455236930@qq.com <zyfqazxsw@qq.com>
2020-08-09 11:30:05 +0800

    增加错误统计

--- source/scikit/service/RecommendMetricUtils.py
+++ source/scikit/service/RecommendMetricUtils.py
@@ -106,3 +106,194 @@ class RecommendMetricUtils:
             recallk[i] = totalRecallScore
             fmeasurek[i] = fmeasure
         return precisonk, recallk, fmeasurek
+
+    @staticmethod
+    def positiveSuccess(recommendCase, answerCase, filterAnswerCase, k=5):
+        """输入参数为多个预测和实际的推荐列表的列表"""
+        positive_success_pr_ratio_k = [0 for x in range(0, k)]
+        positive_success_time_ratio_k = [0 for x in range(0, k)]
+        if recommendCase.__len__() != answerCase.__len__() and answerCase.__len__() != filterAnswerCase.__len__():
+            raise Exception("case is not right")
+
+        for i in range(0, k):
+            positive_success_pr_ratio = 0
+            positive_success_time_ratio = 0
+            for index, recommendList in enumerate(recommendCase):
+                answerList = answerCase[index]
+                filterAnswerList = filterAnswerCase[index]
+                recommendList = recommendList[0:i + 1]
+                positive_success_list = [x for x in recommendList if (x in answerList and x in filterAnswerList)]
+                positive_success_pr_ratio += (positive_success_list.__len__() >= 1)
+                positive_success_time_ratio += positive_success_list.__len__()
+
+            positive_success_pr_ratio /= recommendCase.__len__()
+            positive_success_time_ratio /= recommendCase.__len__() * (i + 1)
+            positive_success_pr_ratio_k[i] = positive_success_pr_ratio
+            positive_success_time_ratio_k[i] = positive_success_time_ratio
+        return positive_success_pr_ratio_k, positive_success_time_ratio_k
+
+    @staticmethod
+    def negativeSuccess(recommendCase, answerCase, filterAnswerCase, k=5):
+        """输入参数为多个预测和实际的推荐列表的列表"""
+        negative_success_pr_ratio_k = [0 for x in range(0, k)]
+        negative_success_time_ratio_k = [0 for x in range(0, k)]
+        if recommendCase.__len__() != answerCase.__len__() and answerCase.__len__() != filterAnswerCase.__len__():
+            raise Exception("case is not right")
+
+        for i in range(0, k):
+            negative_success_pr_ratio = 0
+            negative_success_time_ratio = 0
+            for index, recommendList in enumerate(recommendCase):
+                answerList = answerCase[index]
+                filterAnswerList = filterAnswerCase[index]
+                recommendList = recommendList[0:i + 1]
+                negative_success_list = [x for x in recommendList if (x in answerList and x not in filterAnswerList)]
+                negative_success_pr_ratio += (negative_success_list.__len__() >= 1)
+                negative_success_time_ratio += negative_success_list.__len__()
+
+            negative_success_pr_ratio /= recommendCase.__len__()
+            negative_success_time_ratio /= recommendCase.__len__() * (i + 1)
+            negative_success_pr_ratio_k[i] = negative_success_pr_ratio
+            negative_success_time_ratio_k[i] = negative_success_time_ratio
+        return negative_success_pr_ratio_k, negative_success_time_ratio_k
+
+    @staticmethod
+    def positiveFail(recommendCase, answerCase, filterAnswerCase, k=5):
+        """输入参数为多个预测和实际的推荐列表的列表"""
+        positive_fail_pr_ratio_k = [0 for x in range(0, k)]
+        positive_fail_time_ratio_k = [0 for x in range(0, k)]
+        if recommendCase.__len__() != answerCase.__len__() and answerCase.__len__() != filterAnswerCase.__len__():
+            raise Exception("case is not right")
+
+        for i in range(0, k):
+            positive_fail_pr_ratio = 0
+            positive_fail_time_ratio = 0
+            for index, recommendList in enumerate(recommendCase):
+                answerList = answerCase[index]
+                filterAnswerList = filterAnswerCase[index]
+                recommendList = recommendList[0:i + 1]
+                positive_fail_list = [x for x in recommendList if (x not in answerList and filterAnswerList.__len__() > 0)]
+                positive_fail_pr_ratio += (positive_fail_list.__len__() >= 1)
+                positive_fail_time_ratio += positive_fail_list.__len__()
+
+            positive_fail_pr_ratio /= recommendCase.__len__()
+            positive_fail_time_ratio /= recommendCase.__len__() * (i + 1)
+            positive_fail_pr_ratio_k[i] = positive_fail_pr_ratio
+            positive_fail_time_ratio_k[i] = positive_fail_time_ratio
+        return positive_fail_pr_ratio_k, positive_fail_time_ratio_k
+
+    @staticmethod
+    def negativeFail(recommendCase, answerCase, filterAnswerCase, k=5):
+        """输入参数为多个预测和实际的推荐列表的列表"""
+        negative_fail_pr_ratio_k = [0 for x in range(0, k)]
+        negative_fail_time_ratio_k = [0 for x in range(0, k)]
+        if recommendCase.__len__() != answerCase.__len__() and answerCase.__len__() != filterAnswerCase.__len__():
+            raise Exception("case is not right")
+
+        for i in range(0, k):
+            negative_fail_pr_ratio = 0
+            negative_fail_time_ratio = 0
+            for index, recommendList in enumerate(recommendCase):
+                answerList = answerCase[index]
+                filterAnswerList = filterAnswerCase[index]
+                recommendList = recommendList[0:i + 1]
+                negative_fail_list = [x for x in recommendList if (x not in answerList and filterAnswerList.__len__() == 0)]
+                negative_fail_pr_ratio += (negative_fail_list.__len__() >= 1)
+                negative_fail_time_ratio += negative_fail_list.__len__()
+
+            negative_fail_pr_ratio /= recommendCase.__len__()
+            negative_fail_time_ratio /= recommendCase.__len__() * (i + 1)
+            negative_fail_pr_ratio_k[i] = negative_fail_pr_ratio
+            negative_fail_time_ratio_k[i] = negative_fail_time_ratio
+        return negative_fail_pr_ratio_k, negative_fail_time_ratio_k
+
+    @staticmethod
+    def positiveSuccess2(recommendCase, answerCase, filterAnswerCase, k=5):
+        """输入参数为多个预测和实际的推荐列表的列表
+           判定条件修改
+        """
+        positive_success_pr_ratio_k = [0 for x in range(0, k)]
+        if recommendCase.__len__() != answerCase.__len__() and answerCase.__len__() != filterAnswerCase.__len__():
+            raise Exception("case is not right")
+
+        for i in range(0, k):
+            positive_success_pr_ratio = 0
+            for index, recommendList in enumerate(recommendCase):
+                answerList = answerCase[index]
+                filterAnswerList = filterAnswerCase[index]
+                recommendList = recommendList[0:i + 1]
+                positive_success_list = [x for x in recommendList if (x in answerList and x in filterAnswerList)]
+                positive_success_pr_ratio += (positive_success_list.__len__() >= 1)
+
+            positive_success_pr_ratio /= recommendCase.__len__()
+            positive_success_pr_ratio_k[i] = positive_success_pr_ratio
+        return positive_success_pr_ratio_k
+
+    @staticmethod
+    def negativeSuccess2(recommendCase, answerCase, filterAnswerCase, k=5):
+        """输入参数为多个预测和实际的推荐列表的列表"""
+        negative_success_pr_ratio_k = [0 for x in range(0, k)]
+        if recommendCase.__len__() != answerCase.__len__() and answerCase.__len__() != filterAnswerCase.__len__():
+            raise Exception("case is not right")
+
+        for i in range(0, k):
+            negative_success_pr_ratio = 0
+            for index, recommendList in enumerate(recommendCase):
+                answerList = answerCase[index]
+                filterAnswerList = filterAnswerCase[index]
+                recommendList = recommendList[0:i + 1]
+                # 首先  推荐列表中有人在老答案中
+                # 命中
+                if [x for x in recommendList if x in answerList].__len__() > 0:
+                    negative_success_list = [x for x in recommendList if (x in answerList and x in filterAnswerList)]
+                    negative_success_pr_ratio += (negative_success_list.__len__() == 0)
+
+            negative_success_pr_ratio /= recommendCase.__len__()
+            negative_success_pr_ratio_k[i] = negative_success_pr_ratio
+        return negative_success_pr_ratio_k
+
+    @staticmethod
+    def positiveFail2(recommendCase, answerCase, filterAnswerCase, k=5):
+        """输入参数为多个预测和实际的推荐列表的列表"""
+        positive_fail_pr_ratio_k = [0 for x in range(0, k)]
+        if recommendCase.__len__() != answerCase.__len__() and answerCase.__len__() != filterAnswerCase.__len__():
+            raise Exception("case is not right")
+
+        for i in range(0, k):
+            positive_fail_pr_ratio = 0
+            for index, recommendList in enumerate(recommendCase):
+                answerList = answerCase[index]
+                filterAnswerList = filterAnswerCase[index]
+                recommendList = recommendList[0:i + 1]
+                positive_fail_list = [x for x in recommendList if x in answerList]
+                """一个都没命中"""
+                if positive_fail_list.__len__() == 0 and filterAnswerList.__len__() > 0:
+                    positive_fail_pr_ratio += 1
+
+            positive_fail_pr_ratio /= recommendCase.__len__()
+            positive_fail_pr_ratio_k[i] = positive_fail_pr_ratio
+        return positive_fail_pr_ratio_k
+
+    @staticmethod
+    def negativeFail2(recommendCase, answerCase, filterAnswerCase, k=5):
+        """输入参数为多个预测和实际的推荐列表的列表"""
+        negative_fail_pr_ratio_k = [0 for x in range(0, k)]
+        if recommendCase.__len__() != answerCase.__len__() and answerCase.__len__() != filterAnswerCase.__len__():
+            raise Exception("case is not right")
+
+        for i in range(0, k):
+            negative_fail_pr_ratio = 0
+            for index, recommendList in enumerate(recommendCase):
+                answerList = answerCase[index]
+                filterAnswerList = filterAnswerCase[index]
+                recommendList = recommendList[0:i + 1]
+                """一个都没命中"""
+                negative_fail_list = [x for x in recommendList if x in answerList]
+                if negative_fail_list.__len__() == 0 and filterAnswerList.__len__() == 0:
+                    negative_fail_pr_ratio += 1
+
+            negative_fail_pr_ratio /= recommendCase.__len__()
+            negative_fail_pr_ratio_k[i] = negative_fail_pr_ratio
+        return negative_fail_pr_ratio_k
+
+
